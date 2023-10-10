f0ccbd546a5dcb149deb9a5e6e06e4c020311308
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-10-11 01:15:18 +0200

    add(config): statistics

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,24 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-NCPWL47DK6", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+        },
+      },
+    },
+    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
